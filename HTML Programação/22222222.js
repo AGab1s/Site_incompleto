681e9fa2a42d532ac6ea7550bb153b949cd9d11c
Gabriela Souza <121108279+AGab1s@users.noreply.github.com>
2023-09-24 12:45:44 -0300

    Add files via upload

---
+++ HTML Programação/22222222.js
@@ -0,0 +1,20 @@
+<!DOCTYPE thml>
+<hmtl>
+<head>
+<title>teste javasript<title>
+<meta charset="URD-8" />
+<script>
+function acendeLampada(){
+document.getElementById("Luz").src = "C:\Users\gabri\OneDrive\Imagens\lampada aceza.jpeg";
+}
+fuction apadaLampada(){
+	document.getElementById("Luz"). 
+}
+</script>
+</head>
+<body>
+<h1>Acenda a Lâmpada</h1>
+<img scr= "C:\Users\gabri\OneDrive\Imagens\Lâmpadas-em-png.png" id="Luz" onmousemove="acendaLampada()"/>
+
+</body>
+</html>
